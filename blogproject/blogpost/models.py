9f235d41dc25586e59dfa2fc72474228fd4d3bda
kaede_momiji <44082622+kaedefuto@users.noreply.github.com>
2021-08-22 23:01:26 +0900

    0822

---
+++ blogproject/blogpost/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+
+# Create your models here.
+
+"""
+class SampleModel(models.Model):
+    title=models.CharField(max_length=100)
+    number = models.IntegerField()
+"""
+
+"""
+class Folder(models.Model):
+    title = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.title
+"""
+
+
+#CATEGORY = (('business','ビジネス'),('life','生活'),('other','その他'))
+class BlogModel(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    postdate = models.DateField(auto_now_add=True)
+    category = models.CharField(max_length = 50)
+    #category = models.ForeignKey(Folder, on_delete=models.SET_NULL, null=True)
+    """
+    category = models.CharField(
+        max_length = 50,
+        choices = CATEGORY
+    )
+    """
+    def __str__(self):
+        return self.title
